8db673a325a18da283cdccb03c663da9e980e171
Mukhlas Adib <adib.rasyidy@gmail.com>
2025-10-17 20:34:46 +0700

    next

--- app-01.py
+++ app-01.py
@@ -4,16 +4,26 @@ import os
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+# Minta user masukkan API KEY
 GOOGLE_API_KEY = getpass.getpass("Enter your API key: ")
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
+# Inisiasi client dengan Gemini LLM
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
-messages = [
+# Bikin system prompt
+chat_history = [
     SystemMessage(content="You are a funny assistant that always joking."),
 ]
 
-prompt = input("User: ")
-messages.append(HumanMessage(content=prompt))
-response = llm.invoke(messages)
-print("AI:", response.content)
+while True:
+    # Minta user memasukkan promptnya
+    prompt = input("User: ")
+    chat_history.append(HumanMessage(content=prompt))
+
+    # Masukkan prompt ke LLM, dan print jawabannya
+    response = llm.invoke(chat_history)
+    print()
+    print("AI:", response.content)
+    print()
+    chat_history.append(response)
