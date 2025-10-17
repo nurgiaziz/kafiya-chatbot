6b19d5343a5c8d42965f66a8c52439cc0069f1ce
Mukhlas Adib <adib.rasyidy@gmail.com>
2025-10-17 20:16:54 +0700

    first try

--- README.md
+++
@@ -1,4 +0,0 @@
-# Scripts for AI for Data Science Bootcamp
-
-Written by Mukhlas Adib
-Hacktiv8 in works with AVPN
---
+++ aplikasi_saya.py
@@ -0,0 +1,17 @@
+import getpass
+import os
+
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+GOOGLE_API_KEY = getpass.getpass("Enter your API key: ")
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+messages = [
+    SystemMessage(content="You are a funny assistant that always joking."),
+    HumanMessage(content="What is AI?"),
+]
+response = llm.invoke(messages)
+print(response.content)
