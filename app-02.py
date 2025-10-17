5d65cf6a5aab0292efae431081e14ca0b3332e8a
Mukhlas Adib <adib.rasyidy@gmail.com>
2025-10-17 21:47:00 +0700

    next

--- app-02.py
+++ app-02.py
@@ -1,25 +1,44 @@
-import streamlit as st
+import os
 
-if "GOOGLE_API_KEY" not in st.session_state:
-    st.session_state["GOOGLE_API_KEY"] = ""
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 if "chat_history" not in st.session_state:
     st.session_state["chat_history"] = []
 chat_history = st.session_state["chat_history"]
 
 st.title("ChatBot-Ku")
 
-st.write("Masukkan Google API Key")
 
-col1, col2 = st.columns((80, 20))
-with col1:
-    api_key = st.text_input("", label_visibility="collapsed")
+def get_api_key_input():
+    st.write("Masukkan Google API Key")
 
-with col2:
-    is_submit_pressed = st.button("Submit")
-    if is_submit_pressed:
-        st.session_state["GOOGLE_API_KEY"] = api_key
+    if "GOOGLE_API_KEY" not in st.session_state:
+        st.session_state["GOOGLE_API_KEY"] = ""
 
-GOOGLE_API_KEY = st.session_state["GOOGLE_API_KEY"]
+    col1, col2 = st.columns((80, 20))
+    with col1:
+        api_key = st.text_input("", label_visibility="collapsed", type="password")
+
+    with col2:
+        is_submit_pressed = st.button("Submit")
+        if is_submit_pressed:
+            st.session_state["GOOGLE_API_KEY"] = api_key
+
+    os.environ["GOOGLE_API_KEY"] = st.session_state["GOOGLE_API_KEY"]
+
+
+def load_llm():
+    if "llm" not in st.session_state:
+        st.session_state["llm"] = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+    return st.session_state["llm"]
+
+
+get_api_key_input()
+if not os.environ["GOOGLE_API_KEY"]:
+    st.stop()
+
+llm = load_llm()
 
 prompt = st.chat_input("Chat with AI")
 chat_history.append(prompt)
