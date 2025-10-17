4ee819210ff9832cc9acab545ab62a823822bca7
Mukhlas Adib <adib.rasyidy@gmail.com>
2025-10-17 21:29:12 +0700

    next

--- app-02.py
+++ app-02.py
@@ -1,14 +1,22 @@
 import streamlit as st
 
+if "GOOGLE_API_KEY" not in st.session_state:
+    st.session_state["GOOGLE_API_KEY"] = ""
+
 st.title("ChatBot-Ku")
 
 st.write("Masukkan Google API Key")
+
 col1, col2 = st.columns((80, 20))
 with col1:
-    GOOGLE_API_KEY = st.text_input("", label_visibility="collapsed")
+    api_key = st.text_input("", label_visibility="collapsed")
 
 with col2:
     is_submit_pressed = st.button("Submit")
+    if is_submit_pressed:
+        st.session_state["GOOGLE_API_KEY"] = api_key
 
-if is_submit_pressed:
-    st.write("API KEY Adalah", GOOGLE_API_KEY)
+GOOGLE_API_KEY = st.session_state["GOOGLE_API_KEY"]
+st.write("API KEY Adalah", GOOGLE_API_KEY)
+
+st.button("Random Button")
