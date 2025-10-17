519ebd850416b2737f004f5dfb2792233d83f089
Mukhlas Adib <adib.rasyidy@gmail.com>
2025-10-17 21:08:57 +0700

    next

--- app-02.py
+++ app-02.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("ChatBot-Ku")
+
+st.write("ChatBot bikinan Adib")
+
+GOOGLE_API_KEY = st.text_input("Google API Key")
+is_submit_pressed = st.button("Submit")
+if is_submit_pressed:
+    st.write("API KEY Adalah", GOOGLE_API_KEY)
