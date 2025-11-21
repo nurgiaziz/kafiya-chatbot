ffb1d8a360ce831db81e4e54f14c6cac73a255eb
Mukhlas Adib <adib.rasyidy@gmail.com>
2025-11-21 21:15:01 +0700

    next

--- app3.py
+++ app3.py
@@ -9,21 +9,32 @@ st.title("My ChatBot")
 google_api_key = st.text_input("Google API Key", type="password")
 os.environ["GOOGLE_API_KEY"] = google_api_key
 
-
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
-user_query = st.text_input("Your query:")
-submit = st.button("Submit")
 
-response = None
-if submit:
-    messages_history: list = [
+if "messages_history" not in st.session_state:
+    st.session_state["messages_history"] = [
         SystemMessage(
             "You are a comedian that knows a lot about Bali. Always response in less than 3 sentences in a chat style. Reply in bahasa indonesia"
         )
     ]
-    messages_history.append(HumanMessage(user_query))
-    response = llm.invoke(messages_history)
+messages_history = st.session_state["messages_history"]
 
-if response is not None:
-    st.write(f"AI: {response.content}")
+for message in messages_history:
+    if type(message) is SystemMessage:
+        continue
+    role = "User" if type(message) is HumanMessage else "AI"
+    with st.chat_message(role):
+        st.markdown(message.content)
+
+prompt = st.chat_input("Chat with AI")
+if not prompt:
+    st.stop()
+messages_history.append(HumanMessage(prompt))
+with st.chat_message("User"):
+    st.markdown(prompt)
+
+response = llm.invoke(messages_history)
+messages_history.append(response)
+with st.chat_message("AI"):
+    st.markdown(response.content)
