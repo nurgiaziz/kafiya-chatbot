806717343e392397ce6de45f1d840d7e995a74b4
Mukhlas Adib <adib.rasyidy@gmail.com>
2025-12-19 20:26:37 +0700

    next

---
+++ app2.py
@@ -0,0 +1,22 @@
+import os
+import getpass
+
+from langchain_groq import ChatGroq
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+GROQ_API_KEY = getpass.getpass("Enter your API key: ")
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+llm =  ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct")
+
+chat_history = []
+
+print("Silahkan chat dengan AI anda!!!")
+while True:
+    user_chat = input("User: ")
+    chat_history.append(
+        HumanMessage(user_chat)
+    )
+    response = llm.invoke(chat_history)
+    chat_history.append(response)
+    print(response.content)
