7de4b003519a8a8d3bc0b4f2cf74573a8dc87915
Mukhlas Adib <adib.rasyidy@gmail.com>
2025-12-19 20:03:18 +0700

    next

--- app1.py
+++ app1.py
@@ -1,1 +1,12 @@
-print("Hello guys!")
+import streamlit as st
+
+st.title("Applikasi Adib")
+
+st.markdown("Ini adalah aplikasi bikinan Adib")
+
+user_name = st.text_input("Nama")
+user_age = st.slider("Umur")
+submit_button = st.button("Submit")
+if submit_button:
+    st.markdown(f"Hello {user_name}!")
+    st.markdown(f"Umur {user_age} tahun")
