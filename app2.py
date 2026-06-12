c8141251fdd245834abc506ceb9c384bd928c634
Mukhlas Adib <adib.rasyidy@gmail.com>
2026-06-12 21:17:35 +0700

    next

--- app2.py
+++ app2.py
@@ -5,7 +5,14 @@
 # >>> python -m streamlit run <nama file>
 #
 
+import getpass
+import os
+from inspect import markcoroutinefunction
+
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
 
 st.title("My ChatBot")
 st.markdown("Demo chatbot oleh Mukhlas Adib")
@@ -18,7 +25,25 @@ if st.session_state["api_key"] == "":
     submit_key = st.button("Submit Key")
     if submit_key:
         st.session_state["api_key"] = input_api_key
-    if st.session_state["api_key"] == "":
-        st.stop()
+    if st.session_state["api_key"] != "":
+        st.rerun()
+    st.stop()
 
-st.markdown("Chatbot is ready!")
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+for chat in st.session_state["chat_history"]:
+    if type(chat) is HumanMessage:
+        role = "human"
+    else:
+        role = "ai"
+    with st.chat_message(role):
+        st.markdown(chat.content)
+
+user_input = st.chat_input("Chat here")
+if not user_input:
+    st.stop()
+
+st.session_state["chat_history"].append(HumanMessage(user_input))
+if user_input:
+    st.rerun()
