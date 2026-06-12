c773bf388942e82b56cdb3593993e9433e57328f
Mukhlas Adib <adib.rasyidy@gmail.com>
2026-06-12 21:06:19 +0700

    next

--- app2.py
+++ app2.py
@@ -10,7 +10,15 @@ import streamlit as st
 st.title("My ChatBot")
 st.markdown("Demo chatbot oleh Mukhlas Adib")
 
-api_key = st.text_input("API Key", type="password")
-submit_key = st.button("Submit Key")
-if submit_key:
-    st.markdown("We are ready!")
+if "api_key" not in st.session_state:
+    st.session_state["api_key"] = ""
+
+if st.session_state["api_key"] == "":
+    input_api_key = st.text_input("API Key", type="password")
+    submit_key = st.button("Submit Key")
+    if submit_key:
+        st.session_state["api_key"] = input_api_key
+    if st.session_state["api_key"] == "":
+        st.stop()
+
+st.markdown("Chatbot is ready!")
